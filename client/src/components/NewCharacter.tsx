1c6c5177da5206254738a694d04bb660b2e08785
bpb2008 <brittanythompson08@gmail.com>
2024-02-23 11:36:19 -0600

    add CODEOWNERS file

--- client/src/components/NewCharacter.tsx
+++ client/src/components/NewCharacter.tsx
@@ -1,13 +1,50 @@
 import Box from "@mui/material/Box"; 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography"; 
+import { useState } from "react"; 
 
 const NewCharacter: React.FC = () => {
+    const [character, setCharacter] = useState(""); 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+
+        try {
+            const response = await fetch("http://localhost:3001/addNewCharacter", {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json", 
+                }, 
+                body: JSON.stringify({ character }), 
+            });
+            
+            if (!response.ok) {
+                throw new Error("Failed to add new character");
+            }
+
+            await response.json();
+            setCharacter(""); 
+            //figure out how you want to display this data! It won't be here in this component. 
+
+        } catch (error) {
+            console.error("Oh boy! Error adding new character: ", error); 
+        }
+    }; 
+
     return (
         <Box sx={{margin: "10px"}}>
-            <form>
+            <Box sx={{width: "300px", marginLeft: "150px", marginTop: "15px", marginBottom: "15px"}}>
+            <Typography>Enter the common name of the character below. For example, if you've seen Mickey Mouse, no matter what outfit he is wearing, his generic name "Mickey Mouse" should be entered here.</Typography>
+            </Box>
+            <form onSubmit={handleSubmit}>
                 <div className="form">
-                <TextField sx={{margin: "10px"}} />
+                <TextField 
+                onChange={(e) => setCharacter(e.target.value)}
+                sx={{margin: "10px"}} 
+                value={character} 
+                label="Enter Character Name"
+                />
                 <Button variant="outlined" >Submit New Character</Button>
                 </div>
             </form>
--- client/src/components/NewIndividual.tsx
+++ client/src/components/NewIndividual.tsx
@@ -2,14 +2,29 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import { useState } from "react"; 
 
 const NewIndividual: React.FC = () => {
+    const [nickname, setNickname] = useState(""); 
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <Box>
-            <form>
-                <TextField></TextField>
+        <Box sx={{display: "flex", flexDirection: "column"}}>
+            <Box sx={{width: "300px", marginLeft: "150px", marginTop: "15px", marginBottom: "15px"}}>
+                <Typography>Enter the nickname of a new individual character below. Nicknames are based on a character's specialty outfit. For example, a nickname for Mickey Mouse in his Sorcerer Mickey outfit would be "Sorcerer Mickey"</Typography>
+            </Box>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+                label="Enter character nickname"
+                 /> 
                 <Select></Select>
-                <Button></Button>
+                <Button variant="outlined" >Submit New Individual</Button>
             </form>
         </Box>
     )
